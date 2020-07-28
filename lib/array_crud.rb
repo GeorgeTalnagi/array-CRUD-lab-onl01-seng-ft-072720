ec11afac61c22d3a83e7890e4333dbb5dd7fe5d7
George Talnagi <gtalnagi@yahoo.com>
2020-07-28 18:17:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,13 +27,16 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  colors = ["am", "green", "red", "arrays!"]
+    colors.index(am)
 end
 
 def retrieve_first_element_from_array(array)
-
+  colors = ["blue", "green", "red", "arrays!"]
+    
 end
 
 def retrieve_last_element_from_array(array)
-
+  colors = ["blue", "green", "red", "arrays!"]
+    
 end
