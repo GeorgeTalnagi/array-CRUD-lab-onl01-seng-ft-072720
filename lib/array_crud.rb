7756d27c8eb4b91b3f2872f290adb61d9eb5a6d8
George Talnagi <gtalnagi@yahoo.com>
2020-07-28 18:18:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  colors = ["blue", "green", "red", "arrays!"]
+  colors = ["wow", "green", "red", "arrays!"]
     colors.first
 end
 
