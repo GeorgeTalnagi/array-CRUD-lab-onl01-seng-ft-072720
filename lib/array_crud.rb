5880bd0c8f93a8f85ece51c4335d2dc85b54de01
George Talnagi <gtalnagi@yahoo.com>
2020-07-28 18:09:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   colors = ["blue", "green", "red", "yellow"]
-    colors << "purple"
+    colors << "array"
 end
 
 def add_element_to_start_of_array(array, element)
