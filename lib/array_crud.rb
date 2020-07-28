946bbdbbc94442f2176c19de6ca6aa597f51b51a
George Talnagi <gtalnagi@yahoo.com>
2020-07-28 18:25:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,35 +7,29 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-    array << "arrays!"
+    array <<
 end
 
 def add_element_to_start_of_array(array, element)
-  colors = ["blue", "green", "red", "yellow"]
-    colors.unshift "wow"
+    array.unshift
 end
 
 def remove_element_from_end_of_array(array)
-  colors = ["blue", "green", "red", "arrays!"]
-    colors.pop
+    array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  colors = ["wow", "green", "red", "yellow"]
-    colors.shift
+    array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  colors = ["am", "green", "red", "arrays!"]
-    colors.index()
+  array.index()
 end
 
 def retrieve_first_element_from_array(array)
-  colors = ["wow", "green", "red", "arrays!"]
-    colors.first
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-  colors = ["blue", "green", "red", "arrays!"]
-    colors.last
+  array.last
 end
