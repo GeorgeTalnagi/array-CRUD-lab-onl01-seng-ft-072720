e22828df46c6742851a2c8123ba53bdff1e06fd2
George Talnagi <gtalnagi@yahoo.com>
2020-07-28 18:12:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   colors = ["blue", "green", "red", "yellow"]
-    colors.unshift "arrays!"
+    colors.unshift "wow"
 end
 
 def remove_element_from_end_of_array(array)
